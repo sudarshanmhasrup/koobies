70813cc0aca33b2dd5f8046ef79b8ae6ae59174b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-20 16:14:49 +0530

    Add the "org.jetbrains.compose.hot-reload" plugin to the root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.compose.hot.reload) apply false
 }
 
 // Custom build directory
