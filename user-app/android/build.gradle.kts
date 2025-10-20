1b279e3eb71b456eb3a0cdc6c32c546af4846725
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-20 17:38:16 +0530

    Set Java and Kotlin compatibility to Java 11 for the user-app:android module

--- user-app/android/build.gradle.kts
+++ user-app/android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -14,6 +16,13 @@ android {
         targetSdk = libs.versions.userApp.android.targetSdk.get().toInt()
         versionCode = libs.versions.userApp.android.versionCode.get().toInt()
         versionName = libs.versions.userApp.android.versionName.get()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin.compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
     }
     buildFeatures {
         compose = true
