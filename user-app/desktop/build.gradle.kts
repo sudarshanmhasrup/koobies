947bddb43bc9c37b596da72f8a90519e8e6e20a2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-20 16:11:19 +0530

    Configure Java 21 toolchain and JVM target for user-app:desktop module

--- user-app/desktop/build.gradle.kts
+++ user-app/desktop/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
@@ -8,7 +10,21 @@ group = libs.versions.userApp.desktop.group.get()
 version = libs.versions.userApp.desktop.version.get()
 
 kotlin {
-    jvm()
+    jvm {
+        java {
+            @Suppress("UnstableApiUsage")
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of("21"))
+                vendor.set(JvmVendorSpec.JETBRAINS)
+            }
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
+        }
+
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
 
     sourceSets {
         jvmMain.dependencies {
