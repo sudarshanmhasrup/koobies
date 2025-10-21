ceaaee8e4647606511d89745bf7692e5e287ae63
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-21 20:39:21 +0530

    Initialize Koin in the user-app:android module

--- user-app/android/src/main/kotlin/com/koobies/android/KoobiesApplication.kt
+++ user-app/android/src/main/kotlin/com/koobies/android/KoobiesApplication.kt
@@ -1,9 +1,14 @@
 package com.koobies.android
 
 import android.app.Application
+import koobies.compose.app.di.initKoin
+import org.koin.android.ext.koin.androidContext
 
 class KoobiesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initKoin {
+            androidContext(this@KoobiesApplication)
+        }
     }
 }
