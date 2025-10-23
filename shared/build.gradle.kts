b76f2b77ad99bc589e996a0df24428bc9225e85f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-23 19:54:52 +0530

    Setup Kotlin Multiplatform env for the shared module - Apply plugins. - Define targets: Android, iOS and JVM.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,38 @@
-plugins {
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+group = libs.versions.shared.group.get()
+version = libs.versions.shared.version.get()
+
+kotlin {
+    androidLibrary {
+        namespace = libs.versions.shared.androidLibrary.namespace.get()
+        compileSdk = libs.versions.shared.androidLibrary.compileSdk.get().toInt()
+        minSdk = libs.versions.shared.androidLibrary.minSdk.get().toInt()
+    }
+
+    jvm {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
 }
 
 // Custom build directory
