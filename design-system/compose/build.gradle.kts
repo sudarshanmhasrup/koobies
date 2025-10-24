7b71f13134738ae5054669f28f319a75bab9b0f5
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-24 18:18:26 +0530

    Define the design-system:compose module's android library package information from the version catalog

--- design-system/compose/build.gradle.kts
+++ design-system/compose/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.androidLibrary
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
@@ -9,6 +11,13 @@ group = libs.versions.designSystem.compose.group.get()
 version = libs.versions.designSystem.compose.version.get()
 
 kotlin {
+    @Supress("UnstableApiUsage")
+    androidLibrary {
+        namespace = libs.versions.designSystem.compose.androidLibrary.namespace.get()
+        compileSdk = libs.versions.designSystem.compose.androidLibrary.compileSdk.get().toInt()
+        minSdk = libs.versions.designSystem.compose.androidLibrary.minSdk.get().toInt()
+    }
+
     jvm()
 }
 
