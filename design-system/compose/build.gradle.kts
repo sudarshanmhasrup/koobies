5d57781989ef7ec89f62e2c65fecade0c69aa1c6
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-24 18:14:33 +0530

    Define the design-system:compose module's configuration information from the version catalog

--- design-system/compose/build.gradle.kts
+++ design-system/compose/build.gradle.kts
@@ -1,5 +1,15 @@
 plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+//    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
 
+group = libs.versions.designSystem.compose.group.get()
+version = libs.versions.designSystem.compose.version.get()
+
+kotlin {
+    jvm()
 }
 
 // Custom build directory
