7030a1d7783e879ae0a8c0865795f5eb015cbc68
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-25 00:26:43 +0530

    Add additional navigation options to the HeaderNavigationOptionsList

--- site/src/lib/data/HeaderNavigationOptionsList.ts
+++ site/src/lib/data/HeaderNavigationOptionsList.ts
@@ -2,6 +2,34 @@ const headerNavigationOptionsList: HeaderNavigationOption[] = [
     {
         label: "Home",
         endpoint: "/"
+    },
+    {
+        label: "Internships",
+        endpoint: "/internships"
+    },
+    {
+        label: "Jobs",
+        endpoint: "/jobs"
+    },
+    {
+        label: "Blogs",
+        endpoint: "/blogs"
+    },
+    {
+        label: "Courses",
+        endpoint: "/courses"
+    },
+    {
+        label: "Contributors",
+        endpoint: "/contributors"
+    },
+    {
+        label: "About US",
+        endpoint: "/about-us"
+    },
+    {
+        label: "Find Talent",
+        endpoint: "/find-talent"
     }
 ]
 
