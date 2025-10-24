3877d59fecd236247849b86f93f9ab7eeb0f3f8c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-24 23:54:33 +0530

    Add Header.svelte with basic structure and responsive styling

---
+++ site/src/lib/presentation/components/header/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+    import HeaderLogo from "$lib/presentation/components/header/HeaderLogo.svelte";
+</script>
+<header>
+    <HeaderLogo></HeaderLogo>
+</header>
+<style>
+    header {
+        padding: 0 80px;
+        z-index: 1;
+        width: 100%;
+        position: fixed;
+        background-color: #19191c;
+    }
+
+    @media (max-width: 767px) {
+        header {
+            padding: 0;
+        }
+    }
+</style>
