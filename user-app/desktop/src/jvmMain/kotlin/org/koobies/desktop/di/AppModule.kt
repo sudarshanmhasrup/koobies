8afad04f6f55671953911cc4daea761cc92e7997
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-24 10:18:42 +0530

    Add SplashScreenViewModel to appModule in user-app:desktop module

--- user-app/desktop/src/jvmMain/kotlin/org/koobies/desktop/di/AppModule.kt
+++ user-app/desktop/src/jvmMain/kotlin/org/koobies/desktop/di/AppModule.kt
@@ -1,7 +1,9 @@
 package org.koobies.desktop.di
 
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
+import org.koobies.desktop.presentation.splash.SplashScreenViewModel
 
 internal val appModule = module {
-
+    viewModelOf(::SplashScreenViewModel)
 }
