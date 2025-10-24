e4b51e56f19d921e7ba2ce62815a469040b74f92
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-24 23:52:45 +0530

    Add HeaderLogo.svelte with basic structure and responsive styling

---
+++ site/src/lib/presentation/components/header/HeaderLogo.svelte
@@ -0,0 +1,20 @@
+
+<a href="/">
+    <img src="/koobies-logo.svg" alt="Koobies"/>
+</a>
+<style>
+    a {
+        padding: 28px 40px;
+        display: inline-block;
+    }
+
+    img {
+        height: 28px;
+    }
+
+    @media (max-width: 767px) {
+        a {
+            padding: 20px;
+        }
+    }
+</style>
