c84bbbb03d1bf89e7826f5458d4d94adc8c614b7
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-24 10:17:48 +0530

    Add SplashScreenViewModel to manage splash screen state and progress in user-app:desktop module

---
+++ user-app/desktop/src/jvmMain/kotlin/org/koobies/desktop/presentation/splash/SplashScreenViewModel.kt
@@ -0,0 +1,30 @@
+package org.koobies.desktop.presentation.splash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+internal class SplashScreenViewModel : ViewModel() {
+    var uiState = MutableStateFlow(value = SplashScreenUiState())
+        private set
+
+    init {
+        viewModelScope.launch {
+            // Simulate loading progress
+            for (i in 1..100) {
+                delay(timeMillis = 100)
+                uiState.update { state ->
+                    state.copy(progress = i / 100f)
+                }
+            }
+
+            // Update splash screen as finished
+            uiState.update { state ->
+                state.copy(isFinished = true)
+            }
+        }
+    }
+}
