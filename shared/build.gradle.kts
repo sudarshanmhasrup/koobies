f72d38df1ed3b4c615b75a69281cf048787e9903
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-25 14:30:40 +0530

    Simplify iOS target configuration in shared build script

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,16 +23,9 @@ kotlin {
         }
     }
 
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     sourceSets {
         commonMain.dependencies {
