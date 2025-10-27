b2112d401259457d59413a385f5db631d074aab5
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-27 23:38:38 +0530

    Add the composeAppModifier extension function for reusable Compose modifiers

---
+++ user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/extensions/Modifiers.kt
@@ -0,0 +1,12 @@
+package koobies.compose.app.presentation.extensions
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+internal fun Modifier.composeAppModifier(backgroundColor: Color): Modifier {
+    return this
+        .background(color = backgroundColor)
+        .fillMaxSize()
+}
