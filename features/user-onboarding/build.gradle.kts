12408bfb26a93d49c59a029834b4ff7d68888ad3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-27 13:14:39 +0530

    Add multiplatform configuration to the features:user-onboarding module

--- features/user-onboarding/build.gradle.kts
+++ features/user-onboarding/build.gradle.kts
@@ -1,5 +1,38 @@
-plugins {
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = libs.versions.features.userOnboarding.androidLibrary.namespace.get()
+        compileSdk = libs.versions.features.userOnboarding.androidLibrary.compileSdk.get().toInt()
+        minSdk = libs.versions.features.userOnboarding.androidLibrary.minSdk.get().toInt()
+    }
+
+    jvm {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.bundles.compose.multiplatform)
+        }
+    }
+}
+
+compose.resources {
+    generateResClass = never
 }
 
 // Custom build directory
