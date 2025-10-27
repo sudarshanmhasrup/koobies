dbd256a1442532ffacc5252a810d2b0573515d6e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-27 23:34:18 +0530

    Define separate light and dark mode background colors in shared theme

--- shared/src/commonMain/kotlin/koobies/shared/app/presentation/theme/Colors.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/presentation/theme/Colors.kt
@@ -2,5 +2,8 @@ package koobies.shared.app.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Light mode colors
+internal val backgroundColorLight = Color(0xFFFFFFFF)
+
 // Dark mode colors
-internal val backgroundColor = Color(0xFFFFFFFF)
+internal val backgroundColorDark = Color(0xFF000000)
