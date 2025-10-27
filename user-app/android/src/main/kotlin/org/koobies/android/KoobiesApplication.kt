c4736c99b6eba07a5b71bf523ae524991fb271ce
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-27 11:31:17 +0530

    Remove the self explanatory comments

--- user-app/android/src/main/kotlin/org/koobies/android/KoobiesApplication.kt
+++ user-app/android/src/main/kotlin/org/koobies/android/KoobiesApplication.kt
@@ -4,14 +4,9 @@ import android.app.Application
 import koobies.compose.app.di.initKoin
 import org.koin.android.ext.koin.androidContext
 
-
-/**
- * Main Android application class for Koobies app that handles app initialization
- */
 class KoobiesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        // Initialize the Koin dependency injection framework with Android context
         initKoin {
             androidContext(this@KoobiesApplication)
         }
