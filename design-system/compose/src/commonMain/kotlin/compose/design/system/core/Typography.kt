83e88a37256877facff8883e827031604d0baee4
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-27 13:56:41 +0530

    Add core Typography definitions to design system module

---
+++ design-system/compose/src/commonMain/kotlin/compose/design/system/core/Typography.kt
@@ -0,0 +1,19 @@
+package compose.design.system.core
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Immutable
+internal data class Typography(
+    val bodyLargeRegular: TextStyle = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = 28.sp,
+        letterSpacing = (0.25).sp
+    ),
+    val bodyLargeMedium: TextStyle = bodyLargeRegular.copy(fontWeight = FontWeight.Medium),
+    val bodyLargeSemiBold: TextStyle = bodyLargeRegular.copy(fontWeight = FontWeight.SemiBold),
+    val bodyLargeBold: TextStyle = bodyLargeRegular.copy(fontWeight = FontWeight.Bold)
+)
