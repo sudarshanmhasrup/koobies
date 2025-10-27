d99042b570e865b2e8a795da74a7c7717bc72c68
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-27 14:25:30 +0530

    Add Compose Multiplatform dependencies to design-system module

--- design-system/compose/build.gradle.kts
+++ design-system/compose/build.gradle.kts
@@ -26,6 +26,12 @@ kotlin {
     iosX64()
     iosArm64()
     iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.bundles.compose.multiplatform)
+        }
+    }
 }
 
 // Custom build directory
