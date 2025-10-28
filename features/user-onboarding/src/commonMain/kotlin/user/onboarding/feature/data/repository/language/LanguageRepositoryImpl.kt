fac754b2aa27c30def1606ca00d9d1e191aca0cf
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 21:44:56 +0530

    Rename dataSource to localLanguageDataSource in LanguageRepositoryImpl

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/language/LanguageRepositoryImpl.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/language/LanguageRepositoryImpl.kt
@@ -5,12 +5,12 @@ import user.onboarding.feature.data.datasource.language.LocalLanguageDataSource
 import user.onboarding.feature.domain.model.language.Language
 import user.onboarding.feature.domain.repository.language.LanguageRepository
 
-internal class LanguageRepositoryImpl(private val dataSource: LocalLanguageDataSource) : LanguageRepository {
+internal class LanguageRepositoryImpl(private val localLanguageDataSource: LocalLanguageDataSource) : LanguageRepository {
     override fun getLanguages(): Flow<List<Language>> {
-        return dataSource.getLanguages()
+        return localLanguageDataSource.getLanguages()
     }
 
     override fun updateSelectedLanguage(language: Language) {
-        dataSource.updateSelectedLanguage(language = language)
+        localLanguageDataSource.updateSelectedLanguage(language = language)
     }
 }
