8cbe9173bb1bf76297e3d75b5b681931651a944b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 18:56:41 +0530

    Add an iOS implementation for DataStoreManager in the shared module

---
+++ shared/src/iosMain/kotlin/koobies/shared/app/domain/preferences/DataStoreManager.ios.kt
@@ -0,0 +1,28 @@
+package koobies.shared.app.domain.preferences
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
+
+@OptIn(ExperimentalForeignApi::class)
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class DataStoreManager {
+    actual fun getDataStore(): DataStore<Preferences> {
+        return createDataStore(
+            producePath = {
+                val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+                    directory = NSDocumentDirectory,
+                    inDomain = NSUserDomainMask,
+                    appropriateForURL = null,
+                    create = false,
+                    error = null,
+                )
+                requireNotNull(documentDirectory).path + "/$dataStoreFileName"
+            }
+        )
+    }
+}
