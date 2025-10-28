278a3ac0cb27b7f78ebe5b72b92ea682b465bd5f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 19:33:24 +0530

    Move PreferencesManagerImpl from the domain to the data layer in the shared module

--- shared/src/commonMain/kotlin/koobies/shared/app/di/SharedModule.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/di/SharedModule.kt
@@ -1,7 +1,7 @@
 package koobies.shared.app.di
 
 import koobies.shared.app.domain.preferences.PreferencesManager
-import koobies.shared.app.domain.preferences.PreferencesManagerImpl
+import koobies.shared.app.data.preferences.PreferencesManagerImpl
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
