c3e80912387b7664821af96bbe3a669b5a9de403
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 16:40:01 +0530

    Refactor AppTheme to use the getTypography() function

--- shared/src/commonMain/kotlin/koobies/shared/app/presentation/theme/Theme.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/presentation/theme/Theme.kt
@@ -3,7 +3,6 @@ package koobies.shared.app.presentation.theme
 import androidx.compose.runtime.Composable
 import compose.design.system.api.ComposeAppTheme
 import compose.design.system.core.ColorScheme
-import compose.design.system.core.Typography
 
 // Light mode color scheme
 internal val lightModeColors = ColorScheme(backgroundColor = backgroundColorLight)
@@ -17,9 +16,11 @@ fun KoobiesAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (isDarkMode) darkModeColors else lightModeColors
+    val typography = getTypography()
+
     ComposeAppTheme(
         colorScheme = colorScheme,
-        typography = Typography(),
+        typography = typography,
         content = content
     )
 }
