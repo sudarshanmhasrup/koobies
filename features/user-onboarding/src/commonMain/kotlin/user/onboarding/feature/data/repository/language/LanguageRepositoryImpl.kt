009aca609227a376f87fd7731fd314440765aafd
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 19:59:38 +0530

    Move LanguageRepositoryImpl to the language subpackage in the features:user-onboarding module

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/language/LanguageRepositoryImpl.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/language/LanguageRepositoryImpl.kt
@@ -1,7 +1,7 @@
-package user.onboarding.feature.data.repository
+package user.onboarding.feature.data.repository.language
 
 import kotlinx.coroutines.flow.Flow
-import user.onboarding.feature.data.datasource.LocalLanguageDataSource
+import user.onboarding.feature.data.datasource.language.LocalLanguageDataSource
 import user.onboarding.feature.domain.model.language.Language
 import user.onboarding.feature.domain.repository.language.LanguageRepository
 
