b7db7f73a5e38214f7bfe764123a3933576dc0a2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 22:52:12 +0530

    Add primaryColor to the shared module

--- shared/src/commonMain/kotlin/koobies/shared/app/presentation/theme/Colors.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/presentation/theme/Colors.kt
@@ -2,6 +2,9 @@ package koobies.shared.app.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Common colors
+val primaryColor = Color(0xFF4C5DFF)
+
 // Light mode colors
 internal val backgroundColorLight = Color(0xFFFFFFFF)
 internal val primaryFontColorLight = Color(0xFF181C31)
