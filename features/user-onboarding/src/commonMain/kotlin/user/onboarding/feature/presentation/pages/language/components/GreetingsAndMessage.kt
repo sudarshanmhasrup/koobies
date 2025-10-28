cc1fc46b6e64fa4ed076858a653f077a8111d515
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 17:26:29 +0530

    Add a image composable to the GreetingsAndMessage.kt to show Koodee Welcoming Illustration

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/components/GreetingsAndMessage.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/components/GreetingsAndMessage.kt
@@ -1,5 +1,6 @@
 package user.onboarding.feature.presentation.pages.language.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,12 +8,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import compose.design.system.api.ComposeAppTheme
 import koobies.shared.app.presentation.theme.KoobiesAppTheme
 import koobies.shared.app.resources.Res
+import koobies.shared.app.resources.kodee_welcoming_illustration
 import koobies.shared.app.resources.language_page_greetings
 import koobies.shared.app.resources.language_page_message
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import user.onboarding.feature.presentation.extensions.greetingsAndMessageModifier
@@ -26,14 +30,23 @@ internal fun GreetingsAndMessage(
     val commonModifier = Modifier.fillMaxWidth()
 
     val primaryFontColor = ComposeAppTheme.colorScheme.primaryFontColor
-    val greetingsTextStyle = ComposeAppTheme.typography.displaySmallBold.copy(color = primaryFontColor)
+    val greetingsTextStyle = ComposeAppTheme.typography.displayMediumBold.copy(color = primaryFontColor)
 
     val secondaryFontColor = ComposeAppTheme.colorScheme.secondaryFontColor
-    val messageTextStyle = ComposeAppTheme.typography.bodyLargeMedium.copy(color = secondaryFontColor)
+    val messageTextStyle = ComposeAppTheme.typography.bodyMediumMedium.copy(color = secondaryFontColor)
+
+    val koodeeWelcomingIllustration = painterResource(resource = Res.drawable.kodee_welcoming_illustration)
 
     Column(modifier = modifier) {
+        Image(
+            painter = koodeeWelcomingIllustration,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.height(120.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
         BasicText(text = greetings, style = greetingsTextStyle, modifier = commonModifier)
-        Spacer(modifier = Modifier.height(12.dp))
+        Spacer(modifier = Modifier.height(8.dp))
         BasicText(text = message, style = messageTextStyle, modifier = commonModifier)
     }
 }
