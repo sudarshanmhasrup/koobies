912a3da02a89b70717c7d8c07c120af4928d13c8
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 19:20:54 +0530

    Create sharedModule for dependency injection in the shared module

---
+++ shared/src/commonMain/kotlin/koobies/shared/app/di/SharedModule.kt
@@ -0,0 +1,11 @@
+package koobies.shared.app.di
+
+import koobies.shared.app.domain.preferences.PreferencesManager
+import koobies.shared.app.domain.preferences.PreferencesManagerImpl
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val sharedModule = module {
+    singleOf(::PreferencesManagerImpl).bind<PreferencesManager>()
+}
