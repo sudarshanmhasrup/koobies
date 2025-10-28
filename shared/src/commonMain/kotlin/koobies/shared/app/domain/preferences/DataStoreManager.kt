eeb7bde66fb83f51add602fb2192539320186a6a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 19:05:38 +0530

    Suppress the EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING in DataStoreManager

--- shared/src/commonMain/kotlin/koobies/shared/app/domain/preferences/DataStoreManager.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/domain/preferences/DataStoreManager.kt
@@ -3,6 +3,7 @@ package koobies.shared.app.domain.preferences
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect class DataStoreManager {
     fun getDataStore(): DataStore<Preferences>
 }
