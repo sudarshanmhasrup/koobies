8657f22a27ccf942f77749055df71a5a2834c67d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 19:55:23 +0530

    Create LanguageRepository interface in the features:user-onboarding module

---
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/repository/language/LanguageRepository.kt
@@ -0,0 +1,9 @@
+package user.onboarding.feature.domain.repository.language
+
+import kotlinx.coroutines.flow.Flow
+import user.onboarding.feature.domain.model.language.Language
+
+internal interface LanguageRepository {
+    fun getLanguages(): Flow<List<Language>>
+    fun updateSelectedLanguage(language: Language)
+}
