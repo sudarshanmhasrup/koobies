ad9fbca18fc94ef152e92402d3f5fab4536d6ba4
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 18:55:52 +0530

    Rename DATA_STORE constant to dataStoreFileName in createDataStore.kt

--- shared/src/commonMain/kotlin/koobies/shared/app/domain/preferences/createDataStore.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/domain/preferences/createDataStore.kt
@@ -11,4 +11,4 @@ internal fun createDataStore(producePath: () -> String): DataStore<Preferences> 
     )
 }
 
-internal const val DATA_STORE = "prefs.preferences_pb"
+internal const val dataStoreFileName = "prefs.preferences_pb"
