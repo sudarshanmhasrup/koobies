5dd9d36f73bcc00bcf17f0d0c5d9ed80bc260643
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 22:10:42 +0530

    Create LanguagePageViewModel in the features:user-onboarding module

---
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/LanguagePageViewModel.kt
@@ -0,0 +1,33 @@
+package user.onboarding.feature.presentation.pages.language
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import user.onboarding.feature.domain.model.language.Language
+import user.onboarding.feature.domain.usecase.language.GetLanguagesUseCase
+import user.onboarding.feature.domain.usecase.language.SelectLanguageUseCase
+
+internal class LanguagePageViewModel(
+    private val getLanguagesUseCase: GetLanguagesUseCase,
+    private val selectLanguageUseCase: SelectLanguageUseCase
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(value = LanguagePageUiState())
+    val uiState: StateFlow<LanguagePageUiState> = _uiState
+
+    init {
+        viewModelScope.launch {
+            getLanguagesUseCase.invoke().collect { languages ->
+                _uiState.value = _uiState.value.copy(languages = languages)
+            }
+        }
+    }
+
+    @Suppress("ViewModelPublicFunctionShouldStartWithOn")
+    fun updateSelectedLanguage(language: Language) {
+        viewModelScope.launch {
+            selectLanguageUseCase.invoke(language = language)
+        }
+    }
+}
