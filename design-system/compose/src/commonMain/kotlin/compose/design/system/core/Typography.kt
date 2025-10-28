550a174532b0b9a0cfef00f271b74d974cf13f90
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 14:10:58 +0530

    Add displaySmall typography styles to Typography.kt in design-system:compose module

--- design-system/compose/src/commonMain/kotlin/compose/design/system/core/Typography.kt
+++ design-system/compose/src/commonMain/kotlin/compose/design/system/core/Typography.kt
@@ -7,6 +7,18 @@ import androidx.compose.ui.unit.sp
 
 @Immutable
 data class Typography(
+    // Type: Display Small
+    val displaySmallRegular: TextStyle = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp
+    ),
+    val displaySmallMedium: TextStyle = displaySmallRegular.copy(fontWeight = FontWeight.Medium),
+    val displaySmallSemiBold: TextStyle = displaySmallRegular.copy(fontWeight = FontWeight.SemiBold),
+    val displaySmallBold: TextStyle = displaySmallRegular.copy(fontWeight = FontWeight.Bold),
+
+    // Type: Body Large
     val bodyLargeRegular: TextStyle = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 15.sp,
