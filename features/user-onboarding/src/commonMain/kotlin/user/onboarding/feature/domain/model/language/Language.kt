3b5743c73fdd139f81d1d84d1b18ecba6e6c9996
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-28 19:48:11 +0530

    Refactor Language model to use String for locale instead of Locale object

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/language/Language.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/language/Language.kt
@@ -1,12 +1,8 @@
 package user.onboarding.feature.domain.model.language
 
-import io.github.sudarshanmhasrup.localina.annoations.ExperimentalLocalinaApi
-import io.github.sudarshanmhasrup.localina.api.Locale
-
-@OptIn(ExperimentalLocalinaApi::class)
 internal data class Language(
     val name: String,
     val message: String,
-    val locale: Locale,
+    val locale: String,
     val isSelected: Boolean
 )
