aee8dbc6f29fc3b73cb392a87794fe212766a811
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-29 10:10:58 +0530

    Remove @Composable annotation from Modifier extensions in splashScreenModifier, splashScreenLogoAndContainerModifier, splashScreenProgressBarModifier, and splashScreenLogoModifier

--- user-app/desktop/src/desktopMain/kotlin/org/koobies/desktop/presentation/extensions/Modifiers.kt
+++ user-app/desktop/src/desktopMain/kotlin/org/koobies/desktop/presentation/extensions/Modifiers.kt
@@ -7,14 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-@Composable
 internal fun Modifier.splashScreenModifier(): Modifier {
     return this
         .clip(shape = RoundedCornerShape(size = 12.dp))
@@ -22,21 +20,18 @@ internal fun Modifier.splashScreenModifier(): Modifier {
         .fillMaxSize()
 }
 
-@Composable
 internal fun ColumnScope.splashScreenLogoAndContainerModifier(): Modifier {
     return Modifier
         .fillMaxWidth()
         .weight(1f)
 }
 
-@Composable
 internal fun Modifier.splashScreenProgressBarModifier(): Modifier {
     return this
         .fillMaxWidth()
         .height(6.dp)
 }
 
-@Composable
 internal fun BoxScope.splashScreenLogoModifier(): Modifier {
     return Modifier
         .height(68.dp)
