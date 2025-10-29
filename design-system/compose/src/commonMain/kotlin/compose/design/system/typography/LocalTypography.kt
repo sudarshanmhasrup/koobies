e1d1926520a4844be9131a802c7b7ee7f0bd0686
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-29 14:57:03 +0530

    Add LocalTypography to the design-system module

---
+++ design-system/compose/src/commonMain/kotlin/compose/design/system/typography/LocalTypography.kt
@@ -0,0 +1,7 @@
+package compose.design.system.typography
+
+import androidx.compose.runtime.staticCompositionLocalOf
+
+internal val LocalTypography = staticCompositionLocalOf {
+    Typography()
+}
