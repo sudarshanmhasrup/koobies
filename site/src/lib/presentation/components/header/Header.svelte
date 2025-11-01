4c1db85028d7b3bc73f34ea57dcd43339ec938d5
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-01 15:38:00 +0530

    Add HeaderNavigationOptions to the Header component and update styles

--- site/src/lib/presentation/components/header/Header.svelte
+++ site/src/lib/presentation/components/header/Header.svelte
@@ -1,16 +1,20 @@
 <script>
     import HeaderLogo from "$lib/presentation/components/header/HeaderLogo.svelte";
+    import HeaderNavigationOptions from "$lib/presentation/components/header/HeaderNavigationOptions.svelte";
 </script>
 <header>
     <HeaderLogo></HeaderLogo>
+    <HeaderNavigationOptions></HeaderNavigationOptions>
 </header>
 <style>
     header {
         z-index: 1;
         width: 100%;
-        padding: 0 80px;
+        display: flex;
         position: fixed;
-        background-color: #19191c;
+        padding: 0 200px;
+        align-items: center;
+        justify-content: space-between;
     }
 
     @media (max-width: 767px) {
