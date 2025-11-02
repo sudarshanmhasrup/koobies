4e10df5f0739942a9bb1212589bcc8248d45bd06
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-02 11:37:04 +0530

    Add the language page to the features:user-onboarding module

---
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/LanguagePage.kt
@@ -0,0 +1,120 @@
+package user.onboarding.feature.presentation.pages.language
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.window.core.layout.WindowSizeClass.Companion.HEIGHT_DP_MEDIUM_LOWER_BOUND
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
+import compose.design.system.components.RoundButton
+import koobies.shared.app.presentation.theme.KoobiesAppTheme
+import koobies.shared.app.resources.Res
+import koobies.shared.app.resources.language_page_greetings
+import koobies.shared.app.resources.language_page_message
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
+import user.onboarding.feature.presentation.extensions.continueButtonBackgroundModifier
+import user.onboarding.feature.presentation.extensions.greetingsAndMessageModifier
+import user.onboarding.feature.presentation.pages.language.components.GreetingsAndMessage
+import user.onboarding.feature.presentation.pages.language.components.LanguageCard
+
+@Composable
+fun LanguagePage(modifier: Modifier = Modifier) {
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+
+    val languagePageViewModel = koinViewModel<LanguagePageViewModel>()
+    val isLandscapeMode = windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND) &&
+            !windowSizeClass.isHeightAtLeastBreakpoint(HEIGHT_DP_MEDIUM_LOWER_BOUND)
+
+    if (isLandscapeMode) {
+        Row(modifier = modifier.padding(start = 60.dp)) {
+            val layoutModifier = Modifier
+                .weight(1f)
+                .align(alignment = Alignment.CenterVertically)
+            LanguagePageLayout(
+                isLandscapeMode = isLandscapeMode,
+                languagePageViewModel = languagePageViewModel,
+                modifier = layoutModifier,
+                modifier2 = Modifier.weight(1f),
+            )
+        }
+    } else {
+        Column(modifier = modifier) {
+            LanguagePageLayout(
+                languagePageViewModel = languagePageViewModel,
+                modifier = Modifier.fillMaxWidth(),
+                modifier2 = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+private fun LanguagePageLayout(
+    isLandscapeMode: Boolean = false,
+    languagePageViewModel: LanguagePageViewModel,
+    modifier: Modifier = Modifier,
+    modifier2: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        GreetingsAndMessage(
+            greetings = stringResource(resource = Res.string.language_page_greetings),
+            message = stringResource(resource = Res.string.language_page_message),
+            modifier = Modifier.greetingsAndMessageModifier(isLandscapeMode = isLandscapeMode)
+        )
+    }
+    Box(modifier = modifier2) {
+        LanguageList(
+            languagePageViewModel = languagePageViewModel,
+            modifier = modifier2
+        )
+        // Continue button background
+        Box(modifier = continueButtonBackgroundModifier()) {
+            RoundButton(
+                label = "Continue",
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Composable
+private fun LanguageList(
+    languagePageViewModel: LanguagePageViewModel,
+    modifier: Modifier = Modifier
+) {
+    val uiState = languagePageViewModel.uiState.collectAsStateWithLifecycle()
+    val languages = uiState.value.languages
+
+    LazyColumn(modifier = modifier, verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(items = languages) { language ->
+            LanguageCard(
+                language = language,
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    languagePageViewModel.updateSelectedLanguage(language = language)
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LanguagePagePreview() {
+    KoobiesAppTheme(isDarkMode = false) {
+        LanguagePage(modifier = Modifier.fillMaxSize())
+    }
+}
