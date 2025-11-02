9e38d991693fde3d646a35dfb69dc98b08be438a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-02 09:35:43 +0530

    Remove the Koodee illustration image from the GreetingsAndMessage composable

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/components/GreetingsAndMessage.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/components/GreetingsAndMessage.kt
@@ -1,6 +1,5 @@
 package user.onboarding.feature.presentation.pages.language.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,16 +7,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import compose.design.system.api.Theme
 import koobies.shared.app.presentation.theme.KoobiesAppTheme
 import koobies.shared.app.resources.Res
-import koobies.shared.app.resources.kodee_welcoming_illustration
 import koobies.shared.app.resources.language_page_greetings
 import koobies.shared.app.resources.language_page_message
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import user.onboarding.feature.presentation.extensions.greetingsAndMessageModifier
@@ -37,15 +33,7 @@ internal fun GreetingsAndMessage(
     val secondaryFontColor = Theme.colorScheme.secondaryFontColor
     val messageTextStyle = Theme.typography.bodyMediumMedium.copy(color = secondaryFontColor)
 
-    val koodeeWelcomingIllustration = painterResource(resource = Res.drawable.kodee_welcoming_illustration)
-
     Column(modifier = modifier) {
-        Image(
-            painter = koodeeWelcomingIllustration,
-            contentDescription = null,
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.height(100.dp)
-        )
         Spacer(modifier = Modifier.height(8.dp))
         BasicText(text = greetings, style = greetingsTextStyle, modifier = commonModifier)
         Spacer(modifier = Modifier.height(8.dp))
