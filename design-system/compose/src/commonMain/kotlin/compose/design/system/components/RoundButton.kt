471bf81f0c971e3f44dd2a433c451a87162cab67
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-02 14:10:26 +0530

    Add the RoundButton composable to design system components

---
+++ design-system/compose/src/commonMain/kotlin/compose/design/system/components/RoundButton.kt
@@ -0,0 +1,32 @@
+package compose.design.system.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import compose.design.system.api.Theme
+
+@Composable
+fun RoundButton(
+    label: String,
+    onClick: () -> Unit
+) {
+    val buttonColor = ButtonDefaults.buttonColors(containerColor = Theme.colorScheme.primaryColor)
+    Button(
+        colors = buttonColor,
+        contentPadding = PaddingValues(all = 0.dp),
+        onClick = onClick,
+    ) {
+        Text(
+            text = label,
+            color = Color.White,
+            style = Theme.typography.labelLargeBold,
+            modifier = Modifier.padding(all = 20.dp)
+        )
+    }
+}
