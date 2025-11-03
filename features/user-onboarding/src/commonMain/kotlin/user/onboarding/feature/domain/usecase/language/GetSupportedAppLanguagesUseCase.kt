619b7b5c7f5c6c531123d778d657e86a81eed8c0
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 13:51:52 +0530

    Add GetSupportedAppThemesUseCase and refactor GetSupportedAppLanguagesUseCase for naming consistency

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/usecase/language/GetSupportedAppLanguagesUseCase.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/usecase/language/GetSupportedAppLanguagesUseCase.kt
@@ -4,8 +4,8 @@ import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.domain.model.language.AppLanguage
 import user.onboarding.feature.domain.repository.language.LanguageRepository
 
-internal class GetSupportedAppLanguagesUseCase(private val languageRepository: LanguageRepository) {
+internal class GetSupportedAppLanguagesUseCase(private val repository: LanguageRepository) {
     operator fun invoke(): Flow<List<AppLanguage>> {
-        return languageRepository.getSupportedAppLanguages()
+        return repository.getSupportedAppLanguages()
     }
 }
