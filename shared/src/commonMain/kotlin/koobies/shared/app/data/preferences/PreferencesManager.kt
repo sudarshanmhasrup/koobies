83711fda0ebf780b1b16a0bd01f3857dfa72148b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 15:14:53 +0530

    Add save/get app theme functions to PreferencesManager interface

--- shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManager.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManager.kt
@@ -5,4 +5,7 @@ import kotlinx.coroutines.flow.Flow
 interface PreferencesManager {
     suspend fun saveSelectedAppLanguage(locale: String)
     fun getSelectedAppLanguage(): Flow<String>
+
+    suspend fun saveSelectedAppTheme(theme: String)
+    fun getSelectedAppTheme(): Flow<String>
 }
