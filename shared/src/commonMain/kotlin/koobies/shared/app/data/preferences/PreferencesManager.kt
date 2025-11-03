427d29604fd53164983440163ce75ca368fda90c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 13:06:50 +0530

    Make the saveSelectedAppLanguage function in PreferencesManager a suspend function

--- shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManager.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManager.kt
@@ -3,6 +3,6 @@ package koobies.shared.app.data.preferences
 import kotlinx.coroutines.flow.Flow
 
 internal interface PreferencesManager {
-    fun saveSelectedAppLanguage(locale: String)
+    suspend fun saveSelectedAppLanguage(locale: String)
     fun getSelectedAppLanguage(): Flow<String>
 }
