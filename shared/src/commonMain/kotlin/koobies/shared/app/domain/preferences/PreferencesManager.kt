991588d90e0894365b036b26336e09a76d274d72
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 11:21:50 +0530

    Remove the old PreferencesManager interface from the shared module

--- shared/src/commonMain/kotlin/koobies/shared/app/domain/preferences/PreferencesManager.kt
+++
@@ -1,8 +0,0 @@
-package koobies.shared.app.domain.preferences
-
-import kotlinx.coroutines.flow.Flow
-
-interface PreferencesManager {
-    suspend fun saveSelectedLanguage(locale: String)
-    suspend fun getSelectedLanguage(): Flow<String>
-}
