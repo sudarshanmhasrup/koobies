408f98c1385f8e3f45f73daf0510aba4ee326253
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 11:21:29 +0530

    Rename methods in PreferencesManagerImpl to clarify app language context

--- shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManagerImpl.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManagerImpl.kt
@@ -2,7 +2,6 @@ package koobies.shared.app.data.preferences
 
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import koobies.shared.app.domain.preferences.PreferencesManager
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -10,13 +9,13 @@ class PreferencesManagerImpl : PreferencesManager {
     private val dataStoreManager = DataStoreManager()
     private val dataStore = dataStoreManager.getDataStore()
 
-    override suspend fun saveSelectedLanguage(locale: String) {
+    override suspend fun saveSelectedAppLanguage(locale: String) {
         dataStore.edit { dataStore ->
             dataStore[SELECTED_LANGUAGE_KEY] = locale
         }
     }
 
-    override suspend fun getSelectedLanguage(): Flow<String> {
+    override fun getSelectedAppLanguage(): Flow<String> {
         return dataStore.data.map { dataStore ->
             dataStore[SELECTED_LANGUAGE_KEY] ?: ""
         }
