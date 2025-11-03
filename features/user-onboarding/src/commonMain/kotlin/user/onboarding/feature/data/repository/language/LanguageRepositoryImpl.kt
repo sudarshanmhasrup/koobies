c98f1e92a217acbc82ebc7cace35a19d03e06882
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 11:24:21 +0530

    Refactor: Update package import for PreferencesManager in LanguageRepositoryImpl and update methods name to match the interface contract

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/language/LanguageRepositoryImpl.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/language/LanguageRepositoryImpl.kt
@@ -1,16 +1,16 @@
 package user.onboarding.feature.data.repository.language
 
-import koobies.shared.app.domain.preferences.PreferencesManager
+import koobies.shared.app.data.preferences.PreferencesManager
 import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.data.datasource.local.language.LocalLanguageDataSource
-import user.onboarding.feature.domain.model.language.Language
+import user.onboarding.feature.domain.model.language.AppLanguage
 import user.onboarding.feature.domain.repository.language.LanguageRepository
 
 internal class LanguageRepositoryImpl(
     private val localLanguageDataSource: LocalLanguageDataSource,
     private val userPreferencesManager: PreferencesManager
 ) : LanguageRepository {
-    override fun getLanguages(): Flow<List<Language>> {
+    override fun getLanguages(): Flow<List<AppLanguage>> {
         return localLanguageDataSource.getLanguages()
     }
 
