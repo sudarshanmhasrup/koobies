63f6a2a047892f1bf116662d452f8d8020b780fa
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 11:25:31 +0530

    Rename Language to AppLanguage in LocalLanguageDataSourceImpl and update related method and property names to align with naming conventions

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/language/LocalLanguageDataSourceImpl.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/language/LocalLanguageDataSourceImpl.kt
@@ -1,6 +1,6 @@
 package user.onboarding.feature.data.datasource.local.language
 
-import koobies.shared.app.domain.preferences.PreferencesManager
+import koobies.shared.app.data.preferences.PreferencesManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
@@ -9,68 +9,68 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import user.onboarding.feature.domain.model.language.Language
+import user.onboarding.feature.domain.model.language.AppLanguage
 
 internal class LocalLanguageDataSourceImpl(
     private val userPreferencesManager: PreferencesManager
 ) : LocalLanguageDataSource {
-    private val languageList = MutableStateFlow(
+    private val supportedAppLanguages = MutableStateFlow(
         value = listOf(
-            Language(
+            AppLanguage(
                 name = "English",
                 message = "Use this app in English.",
                 locale = "en",
                 isSelected = true
             ),
-            Language(
+            AppLanguage(
                 name = "Hindi",
                 message = "इस ऐप का उपयोग हिंदी में करें।",
                 locale = "hi",
                 isSelected = false
             ),
-            Language(
+            AppLanguage(
                 name = "Marathi",
                 message = "हे अ‍ॅप मराठीत वापरा.",
                 locale = "mr",
                 isSelected = false
             ),
-            Language(
+            AppLanguage(
                 name = "Spanish",
                 message = "Usa esta aplicación en español.",
                 locale = "es",
                 isSelected = false
             ),
-            Language(
+            AppLanguage(
                 name = "French",
                 message = "Utilisez cette application en français.",
                 locale = "fr",
                 isSelected = false
             ),
-            Language(
+            AppLanguage(
                 name = "German",
                 message = "Verwenden Sie diese App auf Deutsch.",
                 locale = "de",
                 isSelected = false
             ),
-            Language(
+            AppLanguage(
                 name = "Russian",
                 message = "Используйте это приложение на русском.",
                 locale = "ru",
                 isSelected = false
             ),
-            Language(
+            AppLanguage(
                 name = "Chinese (Simplified)",
                 message = "使用简体中文。",
                 locale = "zh",
                 isSelected = false
             ),
-            Language(
+            AppLanguage(
                 name = "Japanese",
                 message = "このアプリを日本語で使う。",
                 locale = "ja",
                 isSelected = false
             ),
-            Language(
+            AppLanguage(
                 name = "Korean",
                 message = "이 앱을 한국어로 사용하세요.",
                 locale = "ko",
@@ -81,21 +81,21 @@ internal class LocalLanguageDataSourceImpl(
 
     init {
         CoroutineScope(Dispatchers.IO).launch {
-            val selectedLanguage = userPreferencesManager.getSelectedLanguage().first()
-            if (selectedLanguage.isEmpty()) {
+            val selectedAppLanguage = userPreferencesManager.getSelectedAppLanguage().first()
+            if (selectedAppLanguage.isEmpty()) {
                 return@launch
             }
 
-            updateSelectedLanguage(locale = selectedLanguage)
+            updateSelectedAppLanguage(locale = selectedAppLanguage)
         }
     }
 
-    override fun getLanguages(): Flow<List<Language>> {
-        return languageList
+    override fun getSupportedAppLanguages(): Flow<List<AppLanguage>> {
+        return supportedAppLanguages
     }
 
-    override suspend fun updateSelectedLanguage(locale: String) {
-        languageList.update { value ->
+    override suspend fun updateSelectedAppLanguage(locale: String) {
+        supportedAppLanguages.update { value ->
             value.map {
                 if (it.locale == locale) {
                     it.copy(isSelected = true)
