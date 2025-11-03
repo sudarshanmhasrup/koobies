aa9a46fea33d5724c2a5a0cd38ce84ad923167c3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 13:10:13 +0530

    Make PreferencesManager.kt public

--- shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManager.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManager.kt
@@ -2,7 +2,7 @@ package koobies.shared.app.data.preferences
 
 import kotlinx.coroutines.flow.Flow
 
-internal interface PreferencesManager {
+interface PreferencesManager {
     suspend fun saveSelectedAppLanguage(locale: String)
     fun getSelectedAppLanguage(): Flow<String>
 }
