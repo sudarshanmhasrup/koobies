85df8806792dc1a8f321209f164cc728e9e09736
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 11:00:11 +0530

    Expose `uiState` as a `StateFlow` in `ComposeAppViewModel` for UI state management

--- shared/src/commonMain/kotlin/koobies/shared/app/presentation/composeApp/ComposeAppViewModel.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/presentation/composeApp/ComposeAppViewModel.kt
@@ -1,7 +1,10 @@
 package koobies.shared.app.presentation.composeApp
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-internal  class ComposeAppViewModel : ViewModel() {
-
+internal class ComposeAppViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(value = ComposeAppUiState())
+    val uiState: StateFlow<ComposeAppUiState> = _uiState
 }
