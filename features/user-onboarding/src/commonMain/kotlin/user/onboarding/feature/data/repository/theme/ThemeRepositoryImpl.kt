a38cb4d37bcfe8b5d02deee7a816cfc06eebf2a6
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 14:04:06 +0530

    Add the ThemeRepositoryImpl implementation to the features:user-onboarding module

---
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/theme/ThemeRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package user.onboarding.feature.data.repository.theme
+
+import androidx.compose.runtime.Composable
+import kotlinx.coroutines.flow.Flow
+import user.onboarding.feature.data.datasource.local.theme.LocalThemeDataSource
+import user.onboarding.feature.domain.model.theme.AppTheme
+import user.onboarding.feature.domain.repository.theme.ThemeRepository
+
+internal class ThemeRepositoryImpl(private val dataSource: LocalThemeDataSource) : ThemeRepository {
+    @Composable
+    override fun getSupportedAppThemes(): Flow<List<AppTheme>> {
+        return dataSource.getSupportedAppThemes()
+    }
+
+    @Composable
+    override fun updateSelectedAppTheme(theme: AppTheme) {
+        dataSource.updateSelectedAppTheme(theme = theme)
+    }
+}
