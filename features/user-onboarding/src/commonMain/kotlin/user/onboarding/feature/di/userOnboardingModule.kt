4a5f15a8f5c9f7ead31f0d47ae83025a283d3ef2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 14:38:42 +0530

    Refactor user-onboarding module to support theme selection

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/di/userOnboardingModule.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/di/userOnboardingModule.kt
@@ -6,16 +6,27 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 import user.onboarding.feature.data.datasource.local.language.LocalLanguageDataSource
 import user.onboarding.feature.data.datasource.local.language.LocalLanguageDataSourceImpl
+import user.onboarding.feature.data.datasource.local.theme.LocalThemeDataSource
+import user.onboarding.feature.data.datasource.local.theme.LocalThemeDataSourceImpl
 import user.onboarding.feature.data.repository.language.LanguageRepositoryImpl
+import user.onboarding.feature.data.repository.theme.ThemeRepositoryImpl
 import user.onboarding.feature.domain.repository.language.LanguageRepository
-import user.onboarding.feature.domain.usecase.language.GetLanguagesUseCase
-import user.onboarding.feature.domain.usecase.language.SelectLanguageUseCase
+import user.onboarding.feature.domain.repository.theme.ThemeRepository
+import user.onboarding.feature.domain.usecase.language.GetSupportedAppLanguagesUseCase
+import user.onboarding.feature.domain.usecase.language.SelectAppLanguageUseCase
+import user.onboarding.feature.domain.usecase.theme.GetSupportedAppThemesUseCase
 import user.onboarding.feature.presentation.pages.language.LanguagePageViewModel
+import user.onboarding.feature.presentation.pages.theme.ThemePageViewModel
 
 val userOnboardingModule = module {
     singleOf(::LocalLanguageDataSourceImpl).bind<LocalLanguageDataSource>()
     singleOf(::LanguageRepositoryImpl).bind<LanguageRepository>()
-    singleOf(::GetLanguagesUseCase)
-    singleOf(::SelectLanguageUseCase)
+    singleOf(::GetSupportedAppLanguagesUseCase)
+    singleOf(::SelectAppLanguageUseCase)
     viewModelOf(::LanguagePageViewModel)
+
+    singleOf(::LocalThemeDataSourceImpl).bind<LocalThemeDataSource>()
+    singleOf(::ThemeRepositoryImpl).bind<ThemeRepository>()
+    singleOf(::GetSupportedAppThemesUseCase)
+    viewModelOf(::ThemePageViewModel)
 }
