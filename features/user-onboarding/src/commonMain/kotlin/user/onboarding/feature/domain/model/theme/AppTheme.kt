53b082c9823f9172cb496e63c47f200480e5bd8d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 13:13:07 +0530

    Add a "message" property to the AppTheme data class

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/theme/AppTheme.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/theme/AppTheme.kt
@@ -2,6 +2,7 @@ package user.onboarding.feature.domain.model.theme
 
 internal data class AppTheme(
     val name: String,
+    val message: String,
     val themeType: ThemeType,
     val isSelected: Boolean
 )
