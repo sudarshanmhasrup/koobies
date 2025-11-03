bc6daa7d87c012af2060c88d792f1204bbe63748
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 12:02:07 +0530

    Refactor LanguagePageUiState to a single line in the features:user-onboarding module

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/LanguagePageUiState.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/LanguagePageUiState.kt
@@ -2,6 +2,4 @@ package user.onboarding.feature.presentation.pages.language
 
 import user.onboarding.feature.domain.model.language.AppLanguage
 
-internal data class LanguagePageUiState(
-    val supportedAppLanguages: List<AppLanguage> = emptyList()
-)
+internal data class LanguagePageUiState(val supportedAppLanguages: List<AppLanguage> = emptyList())
