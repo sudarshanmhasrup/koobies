1d9c0d5242196c95172396e33bfeaff841452c8e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 14:24:57 +0530

    Remove the @Composable annotation from the getSupportedAppThemes and updateSelectedAppTheme functions in the ThemeRepository and its implementation

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/repository/theme/ThemeRepository.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/repository/theme/ThemeRepository.kt
@@ -6,8 +6,6 @@ import user.onboarding.feature.domain.model.theme.AppTheme
 
 @Suppress("ComposableNaming")
 internal interface ThemeRepository {
-    @Composable
     fun getSupportedAppThemes(): Flow<List<AppTheme>>
-    @Composable
     fun updateSelectedAppTheme(theme: AppTheme)
 }
