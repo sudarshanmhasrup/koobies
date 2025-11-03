baf739a2a39daf463453327ba7e7aa86ed9f94ad
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 11:03:51 +0530

    Add shared and user-onboarding modules to Koin in the compose app

--- user-app/compose/src/commonMain/kotlin/koobies/compose/app/di/InitKoin.kt
+++ user-app/compose/src/commonMain/kotlin/koobies/compose/app/di/InitKoin.kt
@@ -1,15 +1,13 @@
 package koobies.compose.app.di
 
+import koobies.shared.app.di.sharedModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
+import user.onboarding.feature.di.userOnboardingModule
 
-/**
- * Initializes Koin dependency injection framework.
- *
- * @param config Optional configuration for Koin application setup. Default is null.
- */
 fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
         config?.invoke(this)
+        modules(sharedModule, userOnboardingModule)
     }
 }
