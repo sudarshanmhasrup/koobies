f32698f3f501274268fd8b59eb33e0cf6b6111b2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 23:08:50 +0530

    Remove the @Suppress("ComposableNaming") annotation from the ThemeRepository interface in the features:user-onboarding module

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/repository/theme/ThemeRepository.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/repository/theme/ThemeRepository.kt
@@ -3,7 +3,6 @@ package user.onboarding.feature.domain.repository.theme
 import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.domain.model.theme.AppTheme
 
-@Suppress("ComposableNaming")
 internal interface ThemeRepository {
     fun getSupportedAppThemes(): Flow<List<AppTheme>>
     suspend fun updateSelectedAppTheme(theme: AppTheme)
