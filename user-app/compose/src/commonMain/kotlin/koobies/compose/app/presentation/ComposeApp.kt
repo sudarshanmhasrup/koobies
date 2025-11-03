b5b0a23458beac2d38bbe5184b601c965ddc7de9
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 11:05:41 +0530

    Create the ComposeApp composable in the user-app module

---
+++ user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/ComposeApp.kt
@@ -0,0 +1,30 @@
+package koobies.compose.app.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import compose.design.system.api.Theme
+import io.github.sudarshanmhasrup.localina.api.LocalinaApp
+import koobies.compose.app.presentation.extensions.composeAppModifier
+import koobies.compose.app.presentation.navigation.ComposeAppNavigation
+import koobies.shared.app.presentation.composeApp.ComposeAppViewModel
+import koobies.shared.app.presentation.theme.KoobiesAppTheme
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+fun ComposeApp() {
+    val composeAppViewModel = koinViewModel<ComposeAppViewModel>()
+    val uiState = composeAppViewModel.uiState.collectAsStateWithLifecycle()
+    val isDarkMode = uiState.value.isDarkMode
+
+    LocalinaApp {
+        KoobiesAppTheme(isDarkMode = isDarkMode) {
+            val backgroundColor = Theme.colorScheme.backgroundColor
+            Box(modifier = Modifier.composeAppModifier(backgroundColor = backgroundColor)) {
+                ComposeAppNavigation(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
