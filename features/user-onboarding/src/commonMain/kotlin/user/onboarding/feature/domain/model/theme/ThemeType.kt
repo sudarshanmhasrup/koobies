57138c6482d1bd329bb6fe4d8cceddb755c8d84b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 15:24:20 +0530

    Add a `code` property to the `ThemeType` enum in the features:user-onboarding module

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/theme/ThemeType.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/theme/ThemeType.kt
@@ -1,5 +1,5 @@
 package user.onboarding.feature.domain.model.theme
 
-internal enum class ThemeType {
-    System, Light, Dark
+internal enum class ThemeType(val code: String) {
+    System(code = "1"), Light(code = "2"), Dark(code = "3")
 }
