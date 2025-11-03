0e934b689af480e124b754a39d9f94140f4526da
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 14:27:48 +0530

    Remove the unused @Composable annotation from the GetSupportedAppThemesUseCase

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/usecase/theme/GetSupportedAppThemesUseCase.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/usecase/theme/GetSupportedAppThemesUseCase.kt
@@ -1,12 +1,10 @@
 package user.onboarding.feature.domain.usecase.theme
 
-import androidx.compose.runtime.Composable
 import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.domain.model.theme.AppTheme
 import user.onboarding.feature.domain.repository.theme.ThemeRepository
 
 internal class GetSupportedAppThemesUseCase(private val themeRepository: ThemeRepository) {
-    @Composable
     operator fun invoke(): Flow<List<AppTheme>> {
         return themeRepository.getSupportedAppThemes()
     }
