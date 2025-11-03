b44d008eead5385c653711bae291a8ceda18de06
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 22:57:47 +0530

    Apply status bars padding to the withDefaultScreen modifier

--- user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/extensions/Modifiers.kt
+++ user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/extensions/Modifiers.kt
@@ -2,6 +2,7 @@ package koobies.compose.app.presentation.extensions
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
@@ -9,4 +10,5 @@ internal fun Modifier.composeAppModifier(backgroundColor: Color): Modifier {
     return this
         .background(color = backgroundColor)
         .fillMaxSize()
+        .statusBarsPadding()
 }
