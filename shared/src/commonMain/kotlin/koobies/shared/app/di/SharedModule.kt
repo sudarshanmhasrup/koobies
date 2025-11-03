6c0b2e76d2be5afaf8deb400df47f0641a83215b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 11:00:22 +0530

    Add ComposeAppViewModel to SharedModule with Koin viewModel definition

--- shared/src/commonMain/kotlin/koobies/shared/app/di/SharedModule.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/di/SharedModule.kt
@@ -2,10 +2,13 @@ package koobies.shared.app.di
 
 import koobies.shared.app.data.preferences.PreferencesManagerImpl
 import koobies.shared.app.domain.preferences.PreferencesManager
+import koobies.shared.app.presentation.composeApp.ComposeAppViewModel
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val sharedModule = module {
     singleOf(::PreferencesManagerImpl).bind<PreferencesManager>()
+    viewModelOf(::ComposeAppViewModel)
 }
