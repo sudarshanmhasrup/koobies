9b873b0d1f15de0acec6c4ddd26c52cd16439311
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 14:37:42 +0530

    Refactor GetSupportedAppThemesUseCase to use a shorter "repository" variable name

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/usecase/theme/GetSupportedAppThemesUseCase.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/usecase/theme/GetSupportedAppThemesUseCase.kt
@@ -4,8 +4,8 @@ import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.domain.model.theme.AppTheme
 import user.onboarding.feature.domain.repository.theme.ThemeRepository
 
-internal class GetSupportedAppThemesUseCase(private val themeRepository: ThemeRepository) {
+internal class GetSupportedAppThemesUseCase(private val repository: ThemeRepository) {
     operator fun invoke(): Flow<List<AppTheme>> {
-        return themeRepository.getSupportedAppThemes()
+        return repository.getSupportedAppThemes()
     }
 }
