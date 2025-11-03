2635931b02be3806aa548a11c7088fadf5f27dc0
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 15:43:49 +0530

    Refactor(user-onboarding): Update ThemeRepositoryImpl to save theme in preferences

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/theme/ThemeRepositoryImpl.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/repository/theme/ThemeRepositoryImpl.kt
@@ -1,16 +1,21 @@
 package user.onboarding.feature.data.repository.theme
 
+import koobies.shared.app.data.preferences.PreferencesManager
 import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.data.datasource.local.theme.LocalThemeDataSource
 import user.onboarding.feature.domain.model.theme.AppTheme
 import user.onboarding.feature.domain.repository.theme.ThemeRepository
 
-internal class ThemeRepositoryImpl(private val dataSource: LocalThemeDataSource) : ThemeRepository {
+internal class ThemeRepositoryImpl(
+    private val dataSource: LocalThemeDataSource,
+    private val preferencesManager: PreferencesManager
+) : ThemeRepository {
     override fun getSupportedAppThemes(): Flow<List<AppTheme>> {
         return dataSource.getSupportedAppThemes()
     }
 
     override suspend fun updateSelectedAppTheme(theme: AppTheme) {
-        dataSource.updateSelectedAppTheme(theme = theme)
+        dataSource.updateSelectedAppTheme(theme = theme.themeType.code)
+        preferencesManager.saveSelectedAppTheme(theme = theme.themeType.code)
     }
 }
