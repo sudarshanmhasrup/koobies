7e658c049813d65c9e24be2d71503513dd43465a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-03 13:06:17 +0530

    Make the PreferencesManager.kt internal

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/theme/LocalThemeDataSource.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/theme/LocalThemeDataSource.kt
@@ -1,9 +1,12 @@
 package user.onboarding.feature.data.datasource.local.theme
 
+import androidx.compose.runtime.Composable
 import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.domain.model.theme.AppTheme
 
 internal interface LocalThemeDataSource {
+    @Composable
     fun getSupportedAppThemes(): Flow<List<AppTheme>>
+    @Composable
     suspend fun updateSelectedAppTheme(theme: AppTheme)
 }
--- shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManager.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManager.kt
@@ -2,7 +2,7 @@ package koobies.shared.app.data.preferences
 
 import kotlinx.coroutines.flow.Flow
 
-interface PreferencesManager {
-    suspend fun saveSelectedAppLanguage(locale: String)
+internal interface PreferencesManager {
+    fun saveSelectedAppLanguage(locale: String)
     fun getSelectedAppLanguage(): Flow<String>
 }
