72e205c6b06670592b72a887bf795469e8260d71
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-04 21:20:08 +0530

    Add dark mode colors and light mode tertiary color to the shared module

--- shared/src/commonMain/kotlin/koobies/shared/app/presentation/theme/Colors.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/presentation/theme/Colors.kt
@@ -9,6 +9,9 @@ val primaryColor = Color(0xFF4C5DFF)
 internal val backgroundColorLight = Color(0xFFFFFFFF)
 internal val primaryFontColorLight = Color(0xFF181C31)
 internal val secondaryFontColorLight = Color(0xFF6B7280)
+internal val tertiaryColorLight = Color(0xFFc7c5d0)
 
 // Dark mode colors
-internal val backgroundColorDark = Color(0xFF000000)
+internal val backgroundColorDark = Color(0xFF0d0e13)
+internal val primaryFontColorDark = Color(0xFFe4e1e9)
+internal val secondaryFontColorDark = Color(0xFFc7c5d0)
