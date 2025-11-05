2b0f98bef1132860406e921a0c4b081bd10455f0
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 23:15:45 +0530

    Refactor: Simplify "LocalThemeDataSourceImpl" by removing "PreferencesManager" dependency and unused logic

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/theme/LocalThemeDataSourceImpl.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/theme/LocalThemeDataSourceImpl.kt
@@ -1,59 +1,20 @@
 package user.onboarding.feature.data.datasource.local.theme
 
-import koobies.shared.app.data.preferences.PreferencesManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.flowOf
 import user.onboarding.feature.domain.model.theme.AppTheme
 import user.onboarding.feature.domain.model.theme.ThemeType
 
-internal class LocalThemeDataSourceImpl(private val preferencesManager: PreferencesManager) : LocalThemeDataSource {
-    private val supportedAppThemes = MutableStateFlow(
+internal class LocalThemeDataSourceImpl : LocalThemeDataSource {
+    private val supportedAppThemes = flowOf(
         listOf(
-            AppTheme(
-                isSelected = true,
-                themeType = ThemeType.System
-            ),
-            AppTheme(
-                isSelected = false,
-                themeType = ThemeType.Light
-            ),
-            AppTheme(
-                isSelected = false,
-                themeType = ThemeType.Dark
-            )
+            AppTheme(themeType = ThemeType.System),
+            AppTheme(themeType = ThemeType.Light),
+            AppTheme(themeType = ThemeType.Dark)
         )
     )
 
-    init {
-        CoroutineScope(Dispatchers.IO).launch {
-            val selectedAppTheme = preferencesManager.getSelectedAppTheme().first()
-            if (selectedAppTheme.isEmpty()) {
-                return@launch
-            }
-
-            updateSelectedAppTheme(theme = selectedAppTheme)
-        }
-    }
-
     override fun getSupportedAppThemes(): Flow<List<AppTheme>> {
         return supportedAppThemes
-    }
-
-    override suspend fun updateSelectedAppTheme(theme: String) {
-        supportedAppThemes.update { value ->
-            value.map {
-                if (it.themeType.code == theme) {
-                    it.copy(isSelected = true)
-                } else {
-                    it.copy(isSelected = false)
-                }
-            }
-        }
     }
 }
