222129ef42214f41aef06458c470c841e0472195
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 16:31:02 +0530

    Refactor LocalLanguageDataSource to be a plain interface and follow Clean Architecture guidelines

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/language/LocalLanguageDataSource.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/language/LocalLanguageDataSource.kt
@@ -1,9 +1,7 @@
 package user.onboarding.feature.data.datasource.local.language
 
-import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.domain.model.language.AppLanguage
 
 internal interface LocalLanguageDataSource {
-    fun getSupportedAppLanguages(): Flow<List<AppLanguage>>
-    suspend fun updateSelectedAppLanguage(locale: String)
+    fun getSupportedAppLanguages(): List<AppLanguage>
 }
