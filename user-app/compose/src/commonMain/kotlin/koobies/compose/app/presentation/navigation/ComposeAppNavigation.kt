5aaa7d55dbf02ea0ef051a2d85fdad6150b84996
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 18:14:05 +0530

    Add the ComposeAppNavigation for managing the main navigation in ComposeApp

---
+++ user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/navigation/ComposeAppNavigation.kt
@@ -0,0 +1,36 @@
+package koobies.compose.app.presentation.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import koobies.shared.app.presentation.navigation.ComposeAppRoute
+import koobies.shared.app.presentation.theme.KoobiesAppTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import user.onboarding.feature.presentation.navigation.UserOnboardingNavigation
+
+@Composable
+internal fun ComposeAppNavigation(modifier: Modifier = Modifier) {
+    val commonSizeModifier = Modifier.fillMaxSize()
+    val composeAppNavHostController = rememberNavController()
+
+    NavHost(
+        startDestination = ComposeAppRoute.UserOnboarding,
+        navController = composeAppNavHostController,
+        modifier = modifier
+    ) {
+        composable<ComposeAppRoute.UserOnboarding> {
+            UserOnboardingNavigation(modifier = commonSizeModifier)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ComposeAppNavigationPreview() {
+    KoobiesAppTheme(isDarkMode = false) {
+        ComposeAppNavigation(modifier = Modifier.fillMaxSize())
+    }
+}
