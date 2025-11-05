bddf234ecd13e017b0454e05038630c4cb371f51
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 21:58:37 +0530

    feat: Initialize `ComposeAppViewModel` with app language and theme preferences

--- shared/src/commonMain/kotlin/koobies/shared/app/presentation/composeApp/ComposeAppViewModel.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/presentation/composeApp/ComposeAppViewModel.kt
@@ -1,10 +1,43 @@
 package koobies.shared.app.presentation.composeApp
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.sudarshanmhasrup.localina.api.LocaleUpdater
+import koobies.shared.app.data.preferences.PreferencesManager
+import koobies.shared.app.presentation.splash.SplashScreenManager
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class ComposeAppViewModel : ViewModel() {
+class ComposeAppViewModel(private val preferencesManager: PreferencesManager) : ViewModel() {
     private val _uiState = MutableStateFlow(value = ComposeAppUiState())
     val uiState: StateFlow<ComposeAppUiState> = _uiState
+
+    init {
+        viewModelScope.launch {
+            val selectedAppLanguage = preferencesManager.getSelectedAppLanguage()
+            val selectedAppTheme = preferencesManager.getSelectedAppTheme()
+
+            combine(selectedAppLanguage, selectedAppTheme) { selectedAppLanguage, selectedAppTheme ->
+                LocaleUpdater.updateLocale(locale = selectedAppLanguage)
+                onChangeTheme(theme = selectedAppTheme)
+            }.collect {
+                SplashScreenManager.markUiAsReady()
+            }
+        }
+    }
+
+    fun onChangeTheme(theme: String) {
+        _uiState.update { value ->
+            value.copy(
+                isDarkMode = when(theme) {
+                    "System" -> true
+                    "Light" -> false
+                    else -> true
+                }
+            )
+        }
+    }
 }
