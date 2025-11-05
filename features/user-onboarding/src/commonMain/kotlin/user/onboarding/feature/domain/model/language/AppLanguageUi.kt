52e8da667c0f84da015876612e849eb390bf0aee
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 17:05:05 +0530

    Rename the `language` property in `AppLanguageUi` to `appLanguage`

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/language/AppLanguageUi.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/language/AppLanguageUi.kt
@@ -1,6 +1,6 @@
 package user.onboarding.feature.domain.model.language
 
 internal data class AppLanguageUi(
-    val language: AppLanguage,
+    val appLanguage: AppLanguage,
     val isSelected: Boolean
 )
