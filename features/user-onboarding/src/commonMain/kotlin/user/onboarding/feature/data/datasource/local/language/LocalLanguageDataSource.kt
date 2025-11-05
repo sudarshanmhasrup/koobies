127286a0fe5def2c45685305d906a7dee98a5940
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 16:36:57 +0530

    Refactor LocalLanguageDataSource to return a Flow of supported languages

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/language/LocalLanguageDataSource.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/language/LocalLanguageDataSource.kt
@@ -1,7 +1,8 @@
 package user.onboarding.feature.data.datasource.local.language
 
+import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.domain.model.language.AppLanguage
 
 internal interface LocalLanguageDataSource {
-    fun getSupportedAppLanguages(): List<AppLanguage>
+    fun getSupportedAppLanguages(): Flow<List<AppLanguage>>
 }
