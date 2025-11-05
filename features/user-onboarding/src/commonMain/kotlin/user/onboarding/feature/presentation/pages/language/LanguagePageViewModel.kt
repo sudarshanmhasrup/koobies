ef392870763ec3447612e9a2e50c88b23274cdaf
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 23:54:05 +0530

    Refactor: Use combine flow to unify supported and selected app languages in LanguagePageViewModel

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/LanguagePageViewModel.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/LanguagePageViewModel.kt
@@ -5,33 +5,57 @@ import androidx.lifecycle.viewModelScope
 import io.github.sudarshanmhasrup.localina.api.LocaleUpdater
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import user.onboarding.feature.domain.model.language.AppLanguage
+import user.onboarding.feature.domain.usecase.language.GetSelectedAppLanguageUseCase
 import user.onboarding.feature.domain.usecase.language.GetSupportedAppLanguagesUseCase
 import user.onboarding.feature.domain.usecase.language.SelectAppLanguageUseCase
+import user.onboarding.feature.presentation.model.language.AppLanguageUi
 
 internal class LanguagePageViewModel(
     private val getSupportedAppLanguagesUseCase: GetSupportedAppLanguagesUseCase,
     private val selectAppLanguageUseCase: SelectAppLanguageUseCase,
+    private val getSelectedAppLanguageUseCase: GetSelectedAppLanguageUseCase
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(value = LanguagePageUiState())
     val uiState: StateFlow<LanguagePageUiState> = _uiState
 
     init {
         viewModelScope.launch {
-            getSupportedAppLanguagesUseCase.invoke().collect { supportedAppLanguages ->
-                _uiState.value = _uiState.value.copy(supportedAppLanguages = supportedAppLanguages)
-
-                val selectedAppLanguage = supportedAppLanguages.find { it.isSelected }
-                LocaleUpdater.updateLocale(locale = selectedAppLanguage?.locale ?: "en")
+            combine(
+                getSupportedAppLanguagesUseCase(),
+                getSelectedAppLanguageUseCase()
+            ) { supportedLanguages, selectedLanguage ->
+                supportedLanguages.map { language ->
+                    AppLanguageUi(
+                        appLanguage = language,
+                        isSelected = language.locale == selectedLanguage
+                    )
+                }
+            }.collect { supportedAppLanguages ->
+                _uiState.update { value ->
+                    value.copy(supportedAppLanguages = supportedAppLanguages)
+                }
             }
         }
     }
 
-    @Suppress("ViewModelPublicFunctionShouldStartWithOn")
-    fun updateSelectedAppLanguage(language: AppLanguage) {
+    fun onLanguageSelected(language: AppLanguage) {
+        _uiState.update { value ->
+            value.copy(supportedAppLanguages = value.supportedAppLanguages.map {
+                it.copy(isSelected = it.appLanguage.locale == language.locale)
+            })
+        }
+        LocaleUpdater.updateLocale(locale = language.locale)
+        onLanguageSelected()
+    }
+
+    fun onLanguageSelected() {
         viewModelScope.launch {
-            selectAppLanguageUseCase.invoke(language = language)
+            val currentSelectedLanguage = _uiState.value.supportedAppLanguages.first { it.isSelected }
+            selectAppLanguageUseCase.invoke(language = currentSelectedLanguage.appLanguage)
         }
     }
 }
