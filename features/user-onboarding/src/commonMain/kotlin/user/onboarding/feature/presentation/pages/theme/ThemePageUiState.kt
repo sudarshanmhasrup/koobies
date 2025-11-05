149a9bac13916a9405746e79b7afc3866bb7a047
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 23:34:34 +0530

    Refactor: Update ThemePageUiState to use AppThemeUi instead of AppTheme

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/theme/ThemePageUiState.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/theme/ThemePageUiState.kt
@@ -1,5 +1,5 @@
 package user.onboarding.feature.presentation.pages.theme
 
-import user.onboarding.feature.domain.model.theme.AppTheme
+import user.onboarding.feature.presentation.model.theme.AppThemeUi
 
-internal data class ThemePageUiState(val supportedAppThemes: List<AppTheme> = emptyList())
+internal data class ThemePageUiState(val supportedAppThemes: List<AppThemeUi> = emptyList())
