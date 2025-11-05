bfe394d9ca2fff1ed4293d12557efc13755a95da
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 16:48:42 +0530

    Remove the `isSelected` property from the `AppLanguage` model

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/language/AppLanguage.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/model/language/AppLanguage.kt
@@ -3,6 +3,5 @@ package user.onboarding.feature.domain.model.language
 internal data class AppLanguage(
     val name: String,
     val message: String,
-    val locale: String,
-    val isSelected: Boolean
+    val locale: String
 )
