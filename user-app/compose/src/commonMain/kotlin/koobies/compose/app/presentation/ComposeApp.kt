089d9832dbdf456f15131c269fc7ecc7669ca14e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 19:17:44 +0530

    Refactor: Use `rememberNavController` to create and remember a `NavController` in `ComposeApp`

--- user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/ComposeApp.kt
+++ user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/ComposeApp.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.rememberNavController
 import compose.design.system.api.Theme
 import io.github.sudarshanmhasrup.localina.api.LocalinaApp
 import koobies.compose.app.presentation.extensions.composeAppModifier
@@ -19,12 +20,16 @@ fun ComposeApp() {
     val composeAppViewModel = koinViewModel<ComposeAppViewModel>()
     val uiState = composeAppViewModel.uiState.collectAsStateWithLifecycle()
     val isDarkMode = uiState.value.isDarkMode
+    val composeAppNavController = rememberNavController()
 
     LocalinaApp {
         KoobiesAppTheme(isDarkMode = isDarkMode) {
             val backgroundColor = Theme.colorScheme.backgroundColor
             Box(modifier = Modifier.composeAppModifier(backgroundColor = backgroundColor)) {
-                ComposeAppNavigation(modifier = Modifier.fillMaxSize())
+                ComposeAppNavigation(
+                    composeAppNavHostController = composeAppNavController,
+                    modifier = Modifier.fillMaxSize()
+                )
                 Box(modifier = navigationBarMaskModifier(backgroundColor = backgroundColor))
             }
         }
