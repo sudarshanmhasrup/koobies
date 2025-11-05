a2385ad2dad2e321a0c136ecf9d49c9d6a7ea8e1
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 15:01:31 +0530

    Add the HeaderNavigationOptions component for rendering navigation options in the header

---
+++ site/src/lib/presentation/components/header/HeaderNavigationOptions.svelte
@@ -0,0 +1,27 @@
+<script>
+    import headerNavigationOptionsList from "$lib/data/HeaderNavigationOptionsList.js";
+    import HeaderNavigationOption from "$lib/presentation/components/header/HeaderNavigationOption.svelte";
+</script>
+<nav>
+    <ul>
+        {#each headerNavigationOptionsList as headerNavigationOption}
+            <HeaderNavigationOption headerNavigationOption={headerNavigationOption}></HeaderNavigationOption>
+        {/each}
+    </ul>
+</nav>
+<style>
+    nav {
+        height: 100%;
+    }
+
+    ul {
+        height: 100%;
+        display: flex;
+    }
+
+    @media screen and (max-width: 1200px) {
+        nav {
+            display: none;
+        }
+    }
+</style>
