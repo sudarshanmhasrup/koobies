9da5ef148ba0e2712ee5ddf0455479edec57d2a4
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 23:14:49 +0530

    Refactor: Remove unused "updateSelectedAppTheme" function from "LocalThemeDataSource" interface

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/theme/LocalThemeDataSource.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/theme/LocalThemeDataSource.kt
@@ -3,8 +3,6 @@ package user.onboarding.feature.data.datasource.local.theme
 import kotlinx.coroutines.flow.Flow
 import user.onboarding.feature.domain.model.theme.AppTheme
 
-@Suppress("ComposableNaming")
 internal interface LocalThemeDataSource {
     fun getSupportedAppThemes(): Flow<List<AppTheme>>
-    suspend fun updateSelectedAppTheme(theme: String)
 }
