590211ebcafc5d91b81eb35cf0231fba8a284d43
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 18:10:27 +0530

    Rename `ComposeAppRoutes` to `ComposeAppRoute` and replace `object` with `data object` in `UserOnboarding` for consistency.

--- shared/src/commonMain/kotlin/koobies/shared/app/presentation/navigation/ComposeAppRoute.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/presentation/navigation/ComposeAppRoute.kt
@@ -2,7 +2,7 @@ package koobies.shared.app.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
-sealed class ComposeAppRoutes {
+sealed class ComposeAppRoute {
     @Serializable
-    object UserOnboarding : ComposeAppRoutes()
+    data object UserOnboarding : ComposeAppRoute()
 }
