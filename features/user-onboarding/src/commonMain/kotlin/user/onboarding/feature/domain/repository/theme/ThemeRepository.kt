ce530a0e2bb534832f8eb5391027bbbb2c44338a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 23:20:22 +0530

    feat: Add "getSelectedAppTheme" function to "ThemeRepository"

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/repository/theme/ThemeRepository.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/domain/repository/theme/ThemeRepository.kt
@@ -6,4 +6,5 @@ import user.onboarding.feature.domain.model.theme.AppTheme
 internal interface ThemeRepository {
     fun getSupportedAppThemes(): Flow<List<AppTheme>>
     suspend fun updateSelectedAppTheme(theme: AppTheme)
+    fun getSelectedAppTheme(): Flow<String>
 }
