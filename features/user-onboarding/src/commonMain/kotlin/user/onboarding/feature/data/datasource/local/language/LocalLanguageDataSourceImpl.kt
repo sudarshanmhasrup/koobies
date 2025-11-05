9a7b11f20f4e4c3a05033868bce3c4e9b49de324
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 16:37:05 +0530

    Refactor LocalLanguageDataSource to return a Flow of app languages

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/language/LocalLanguageDataSourceImpl.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/data/datasource/local/language/LocalLanguageDataSourceImpl.kt
@@ -1,72 +1,66 @@
 package user.onboarding.feature.data.datasource.local.language
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import user.onboarding.feature.domain.model.language.AppLanguage
 
 internal class LocalLanguageDataSourceImpl : LocalLanguageDataSource {
-    private val supportedAppLanguages = listOf(
-        AppLanguage(
-            name = "English",
-            message = "Use this app in English.",
-            locale = "en",
-            isSelected = true
-        ),
-        AppLanguage(
-            name = "Hindi",
-            message = "इस ऐप का उपयोग हिंदी में करें।",
-            locale = "hi",
-            isSelected = false
-        ),
-        AppLanguage(
-            name = "Marathi",
-            message = "हे अ‍ॅप मराठीत वापरा.",
-            locale = "mr",
-            isSelected = false
-        ),
-        AppLanguage(
-            name = "Spanish",
-            message = "Usa esta aplicación en español.",
-            locale = "es",
-            isSelected = false
-        ),
-        AppLanguage(
-            name = "French",
-            message = "Utilisez cette application en français.",
-            locale = "fr",
-            isSelected = false
-        ),
-        AppLanguage(
-            name = "German",
-            message = "Verwenden Sie diese App auf Deutsch.",
-            locale = "de",
-            isSelected = false
-        ),
-        AppLanguage(
-            name = "Russian",
-            message = "Используйте это приложение на русском.",
-            locale = "ru",
-            isSelected = false
-        ),
-        AppLanguage(
-            name = "Chinese (Simplified)",
-            message = "使用简体中文。",
-            locale = "zh",
-            isSelected = false
-        ),
-        AppLanguage(
-            name = "Japanese",
-            message = "このアプリを日本語で使う。",
-            locale = "ja",
-            isSelected = false
-        ),
-        AppLanguage(
-            name = "Korean",
-            message = "이 앱을 한국어로 사용하세요.",
-            locale = "ko",
-            isSelected = false
+    private val supportedAppLanguages = flowOf(
+        value = listOf(
+            AppLanguage(
+                name = "English",
+                message = "Use this app in English.",
+                locale = "en"
+            ),
+            AppLanguage(
+                name = "Hindi",
+                message = "इस ऐप का उपयोग हिंदी में करें।",
+                locale = "hi"
+            ),
+            AppLanguage(
+                name = "Marathi",
+                message = "हे अ‍ॅप मराठीत वापरा.",
+                locale = "mr"
+            ),
+            AppLanguage(
+                name = "Spanish",
+                message = "Usa esta aplicación en español.",
+                locale = "es"
+            ),
+            AppLanguage(
+                name = "French",
+                message = "Utilisez cette application en français.",
+                locale = "fr"
+            ),
+            AppLanguage(
+                name = "German",
+                message = "Verwenden Sie diese App auf Deutsch.",
+                locale = "de"
+            ),
+            AppLanguage(
+                name = "Russian",
+                message = "Используйте это приложение на русском.",
+                locale = "ru"
+            ),
+            AppLanguage(
+                name = "Chinese (Simplified)",
+                message = "使用简体中文。",
+                locale = "zh"
+            ),
+            AppLanguage(
+                name = "Japanese",
+                message = "このアプリを日本語で使う。",
+                locale = "ja"
+            ),
+            AppLanguage(
+                name = "Korean",
+                message = "이 앱을 한국어로 사용하세요.",
+                locale = "ko"
+            )
         )
     )
 
-    override fun getSupportedAppLanguages(): List<AppLanguage> {
+    override fun getSupportedAppLanguages(): Flow<List<AppLanguage>> {
         return supportedAppLanguages
     }
 }
