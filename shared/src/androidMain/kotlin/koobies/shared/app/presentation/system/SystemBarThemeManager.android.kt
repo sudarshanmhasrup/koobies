cd767383a1ddd50d2407b5bea9f7ac0368efdf22
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-05 22:51:00 +0530

    Fix: Correct Koin DI type for injecting Activity in `SystemBarThemeManager`

--- shared/src/androidMain/kotlin/koobies/shared/app/presentation/system/SystemBarThemeManager.android.kt
+++ shared/src/androidMain/kotlin/koobies/shared/app/presentation/system/SystemBarThemeManager.android.kt
@@ -1,14 +1,12 @@
 package koobies.shared.app.presentation.system
 
 import android.app.Activity
-import android.content.Context
 import androidx.core.view.WindowCompat
 import org.koin.java.KoinJavaComponent
-import kotlin.getValue
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 internal actual class SystemBarThemeManager {
-    private val activity: Activity by KoinJavaComponent.inject(Context::class.java)
+    private val activity: Activity by KoinJavaComponent.inject(Activity::class.java)
     actual fun setDarkMode() {
         WindowCompat.getInsetsController(activity.window, activity.window.decorView).apply {
             isAppearanceLightStatusBars = false
