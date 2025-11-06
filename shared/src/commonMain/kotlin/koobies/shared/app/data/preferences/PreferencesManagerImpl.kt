598945dd5af58ebb5f7e66a1c06c59ad1f5c09c6
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-06 15:23:59 +0530

    Refactor PreferencesManagerImpl to use Koin for dependency injection

--- shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManagerImpl.kt
+++ shared/src/commonMain/kotlin/koobies/shared/app/data/preferences/PreferencesManagerImpl.kt
@@ -4,9 +4,11 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class PreferencesManagerImpl : PreferencesManager {
-    private val dataStoreManager = DataStoreManager()
+class PreferencesManagerImpl : PreferencesManager, KoinComponent {
+    private val dataStoreManager: DataStoreManager by inject()
     private val dataStore = dataStoreManager.getDataStore()
 
     override suspend fun saveSelectedAppLanguage(locale: String) {
