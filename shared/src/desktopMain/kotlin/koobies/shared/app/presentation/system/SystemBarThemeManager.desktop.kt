28c7cd8a2f23d04ec43b584914072c2bdf4f8a10
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-06 15:38:05 +0530

    Create a no-op implementation of `SystemBarThemeManager` for desktop

---
+++ shared/src/desktopMain/kotlin/koobies/shared/app/presentation/system/SystemBarThemeManager.desktop.kt
@@ -0,0 +1,12 @@
+package koobies.shared.app.presentation.system
+
+@Suppress(names = ["EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING"])
+internal actual class SystemBarThemeManager {
+    actual fun setDarkMode() {
+        TODO("No implementation needed as Desktop doesn't have navigation or status bars.")
+    }
+
+    actual fun setLightMode() {
+        TODO("No implementation needed as Desktop doesn't have navigation or status bars.")
+    }
+}
