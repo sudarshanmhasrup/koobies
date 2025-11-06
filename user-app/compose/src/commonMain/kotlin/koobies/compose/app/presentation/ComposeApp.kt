175cff85642b09e68b105e6e13d4fa2b6f7a736c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-07 01:24:42 +0530

    Inject NavHostController into ComposeAppNavigation for better navigation management

--- user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/ComposeApp.kt
+++ user-app/compose/src/commonMain/kotlin/koobies/compose/app/presentation/ComposeApp.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.rememberNavController
 import compose.design.system.api.Theme
 import io.github.sudarshanmhasrup.localina.api.LocalinaApp
 import koobies.compose.app.presentation.extensions.composeAppModifier
@@ -20,11 +21,13 @@ fun ComposeApp() {
     val uiState = composeAppViewModel.uiState.collectAsStateWithLifecycle()
     val isDarkMode = uiState.value.isDarkMode
 
+    val composeAppNavHostController = rememberNavController()
+
     LocalinaApp {
         KoobiesAppTheme(isDarkMode = isDarkMode) {
             val backgroundColor = Theme.colorScheme.backgroundColor
             Box(modifier = Modifier.composeAppModifier(backgroundColor = backgroundColor)) {
-                ComposeAppNavigation(modifier = Modifier.fillMaxSize())
+                ComposeAppNavigation(composeAppNavHostController = composeAppNavHostController, modifier = Modifier.fillMaxSize())
                 Box(modifier = navigationBarMaskModifier(backgroundColor = backgroundColor))
             }
         }
