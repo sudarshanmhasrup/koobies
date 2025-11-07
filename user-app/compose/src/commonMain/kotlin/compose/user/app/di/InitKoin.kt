3ecbe23fa1db1c114b24728350e87d744716f7e6
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-07 14:26:39 +0530

    Add KDoc for "initKoin" function in InitKoin.kt

--- user-app/compose/src/commonMain/kotlin/compose/user/app/di/InitKoin.kt
+++ user-app/compose/src/commonMain/kotlin/compose/user/app/di/InitKoin.kt
@@ -5,6 +5,18 @@ import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 import user.onboarding.feature.di.userOnboardingModule
 
+/**
+ * Starts Koin for the app and loads shared + platform modules.
+ *
+ * @param config Optional block to set up custom Koin settings (e.g., Android context).
+ *
+ * Example:
+ * ```
+ * initKoin {
+ *     androidContext(context)
+ * }
+ * ```
+ */
 fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
         config?.invoke(this)
