d61ae841b1a30ead952056aa7c4dfa8490e820c9
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-07 14:21:25 +0530

    Remove the unused `CustomTitleBar` and `CustomTitle` composables

--- user-app/desktop/src/desktopMain/kotlin/org/koobies/desktop/presentation/screens/main/MainScreen.kt
+++ user-app/desktop/src/desktopMain/kotlin/org/koobies/desktop/presentation/screens/main/MainScreen.kt
@@ -1,32 +1,21 @@
 package org.koobies.desktop.presentation.screens.main
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.rememberWindowState
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import compose.design.system.api.Theme
-import compose.user.app.DesktopApp
 import compose.shared.app.presentation.theme.KoobiesAppTheme
 import compose.shared.app.resources.Res
 import compose.shared.app.resources.ic_koobies_desktop
-import compose.shared.app.resources.koobies_logo
+import compose.user.app.DesktopApp
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.koobies.desktop.presentation.extensions.splashScreenModifier
 import org.koobies.desktop.presentation.screens.splash.SplashScreen
 import org.koobies.desktop.presentation.screens.splash.SplashScreenViewModel
-import javax.swing.JFrame
 import kotlin.system.exitProcess
 
 @Composable
@@ -55,29 +44,8 @@ private fun MainWindow() {
     ) {
         KoobiesAppTheme {
             Column(modifier = Modifier.fillMaxSize()) {
-//                CustomTitleBar(window = window, modifier = Modifier.fillMaxWidth())
                 DesktopApp()
             }
         }
-    }
-}
-
-@Composable
-fun CustomTitleBar(window: JFrame, modifier: Modifier = Modifier) {
-    Row(modifier = modifier) {
-        CustomTitle(modifier = Modifier.padding(vertical = 16.dp, horizontal = 20.dp))
-        Spacer(modifier = Modifier.weight(1f))
-    }
-}
-
-@Composable
-private fun CustomTitle(modifier: Modifier = Modifier) {
-    Box(modifier = modifier) {
-        Image(
-            painter = painterResource(resource = Res.drawable.koobies_logo),
-            contentDescription = "Koobies Logo",
-            colorFilter = ColorFilter.tint(color = Theme.colorScheme.primaryFontColor),
-            modifier = Modifier.height(20.dp)
-        )
     }
 }
