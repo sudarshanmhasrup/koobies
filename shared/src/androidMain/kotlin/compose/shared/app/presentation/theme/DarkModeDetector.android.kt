5f0c75bc1f068ebda9bb548372dcbe4ac0085149
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-07 14:38:39 +0530

    Refactor: Update "KoinJavaComponent.inject" usage in DarkModeDetector for clarity

--- shared/src/androidMain/kotlin/compose/shared/app/presentation/theme/DarkModeDetector.android.kt
+++ shared/src/androidMain/kotlin/compose/shared/app/presentation/theme/DarkModeDetector.android.kt
@@ -5,7 +5,7 @@ import android.content.res.Configuration
 import org.koin.java.KoinJavaComponent
 
 internal actual fun isSystemInDarkMode(): Boolean {
-    val context: Context by KoinJavaComponent.inject(Context::class.java)
+    val context: Context by KoinJavaComponent.inject(clazz = Context::class.java)
     val configuration = context.resources.configuration
     val currentNightMode = configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
     return currentNightMode == Configuration.UI_MODE_NIGHT_YES
