e56663cf73b7eabbc00dc3d7a443ad84d3dc7bb2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-07 11:59:36 +0530

    Implement the Android-specific logic for "isSystemInDarkMode"

---
+++ shared/src/androidMain/kotlin/koobies/shared/app/presentation/theme/DarkModeDetector.android.kt
@@ -0,0 +1,12 @@
+package koobies.shared.app.presentation.theme
+
+import android.content.Context
+import android.content.res.Configuration
+import org.koin.java.KoinJavaComponent
+
+internal actual fun isSystemInDarkMode(): Boolean {
+    val context: Context by KoinJavaComponent.inject(Context::class.java)
+    val configuration = context.resources.configuration
+    val currentNightMode = configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+    return currentNightMode == Configuration.UI_MODE_NIGHT_YES
+}
