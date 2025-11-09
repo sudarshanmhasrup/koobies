51d08d862162247f812ccf79e4008d58b2b1dd87
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-09 18:23:38 +0530

    Add welcoming illustration to GreetingsAndMessage component in user onboarding

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/components/GreetingsAndMessage.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/components/GreetingsAndMessage.kt
@@ -1,5 +1,6 @@
 package user.onboarding.feature.presentation.pages.language.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,8 +13,10 @@ import androidx.compose.ui.unit.dp
 import compose.design.system.api.Theme
 import compose.shared.app.presentation.theme.KoobiesAppTheme
 import compose.shared.app.resources.Res
+import compose.shared.app.resources.kodee_welcoming_illustration
 import compose.shared.app.resources.language_page_greetings
 import compose.shared.app.resources.language_page_message
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import user.onboarding.feature.presentation.extensions.greetingsAndMessageModifier
@@ -34,6 +37,11 @@ internal fun GreetingsAndMessage(
     val messageTextStyle = Theme.typography.bodyMediumMedium.copy(color = secondaryFontColor)
 
     Column(modifier = modifier) {
+        Image(
+            painter = painterResource(resource = Res.drawable.kodee_welcoming_illustration),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(20.dp))
         BasicText(text = greetings, style = greetingsTextStyle, modifier = commonWidthModifier)
         Spacer(modifier = Modifier.height(12.dp))
         BasicText(text = message, style = messageTextStyle, modifier = commonWidthModifier)
