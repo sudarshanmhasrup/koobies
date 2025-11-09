bd841b22130aa6c0da916cd142904615b25d4324
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-09 10:40:50 +0530

    Refactor "userOnboardingNavigationCardModifier" to support customizable background color and clean up unused imports

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/extensions/Modifiers.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/extensions/Modifiers.kt
@@ -10,34 +10,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import compose.design.system.api.Theme
+
+@Composable
+internal fun BoxScope.userOnboardingNavigationCardModifier(backgroundColor: Color): Modifier {
+    val cardShape = RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp)
+    return Modifier
+        .fillMaxWidth(0.32f)
+        .padding(top = 80.dp)
+        .align(alignment = Alignment.Center)
+        .clip(shape = cardShape)
+        .background(color = backgroundColor)
+        .padding(all = 12.dp)
+}
 
 internal fun Modifier.greetingsAndMessageModifier(isLandscapeMode: Boolean = false): Modifier {
     val topPadding = if (isLandscapeMode) 20.dp else 80.dp
     return this
         .fillMaxWidth()
         .padding(top = topPadding, bottom = 0.dp, start = 20.dp, end = 20.dp)
-}
-
-@Composable
-internal fun BoxScope.userOnboardingNavigationCardModifier(): Modifier {
-    val cardShape = RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp)
-    return Modifier
-        .fillMaxWidth(0.32f)
-        .padding(top = 80.dp)
-        .align(alignment = Alignment.Center)
-        .shadow(
-            elevation = 24.dp,
-            shape = cardShape,
-            ambientColor = Color(0xFF777680).copy(alpha = 0.94f),
-            spotColor = Color.Black.copy(alpha = 0.12f)
-        )
-        .clip(shape = cardShape)
-        .background(color = Theme.colorScheme.backgroundColor)
-        .padding(all = 12.dp)
 }
 
 @Composable
