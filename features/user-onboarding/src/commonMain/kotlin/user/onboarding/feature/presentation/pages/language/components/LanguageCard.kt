3fce2292dbc550af48038bff56388660727be094
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-10 22:16:46 +0530

    Remove unused modifiers and imports from LanguageCard component

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/components/LanguageCard.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/language/components/LanguageCard.kt
@@ -1,21 +1,15 @@
 package user.onboarding.feature.presentation.pages.language.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -29,17 +23,9 @@ import user.onboarding.feature.presentation.model.language.AppLanguageUi
 internal fun LanguageCard(
     language: AppLanguageUi,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     val commonWidthModifier = Modifier.fillMaxWidth()
-    val backgroundModifier = modifier
-        .background(color = Theme.colorScheme.backgroundColor)
-        .clickable(
-            onClick = onClick,
-            interactionSource = remember { MutableInteractionSource() },
-            indication = ripple()
-        )
-        .padding(vertical = 12.dp, horizontal = 8.dp)
 
     val radioButtonColors = RadioButtonDefaults.colors(
         selectedColor = Theme.colorScheme.primaryColor,
@@ -52,7 +38,7 @@ internal fun LanguageCard(
     val secondaryFontColor = Theme.colorScheme.secondaryFontColor
     val messageTextStyle = Theme.typography.bodyMediumMedium.copy(color = secondaryFontColor)
 
-    Row(verticalAlignment = Alignment.CenterVertically, modifier = backgroundModifier) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
         RadioButton(selected = language.isSelected, colors = radioButtonColors, onClick = onClick)
         Spacer(modifier = Modifier.width(8.dp))
         Column(modifier = Modifier.fillMaxWidth()) {
