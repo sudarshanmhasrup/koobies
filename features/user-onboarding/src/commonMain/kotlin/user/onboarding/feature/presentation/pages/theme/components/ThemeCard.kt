fc212993a89a2a00bc057ac71c0791311676788e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-10 22:20:02 +0530

    Remove unused imports from ThemeCard component

--- features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/theme/components/ThemeCard.kt
+++ features/user-onboarding/src/commonMain/kotlin/user/onboarding/feature/presentation/pages/theme/components/ThemeCard.kt
@@ -1,21 +1,15 @@
 package user.onboarding.feature.presentation.pages.theme.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
