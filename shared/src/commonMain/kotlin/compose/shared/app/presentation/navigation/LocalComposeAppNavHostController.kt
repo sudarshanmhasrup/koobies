23e04d4fe4a7001bce4b2e782528558a164bce0f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-11 16:08:50 +0530

    Rename ComposeAppNavHostController to LocalComposeAppNavHostController for clarity

--- shared/src/commonMain/kotlin/compose/shared/app/presentation/navigation/LocalComposeAppNavHostController.kt
+++ shared/src/commonMain/kotlin/compose/shared/app/presentation/navigation/LocalComposeAppNavHostController.kt
@@ -3,6 +3,6 @@ package compose.shared.app.presentation.navigation
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.navigation.NavHostController
 
-val ComposeAppNavHostController = staticCompositionLocalOf<NavHostController> {
+val LocalComposeAppNavHostController = staticCompositionLocalOf<NavHostController> {
     error(message = "No NavHostController provided for the ComposeApp module's navigation host.")
 }
