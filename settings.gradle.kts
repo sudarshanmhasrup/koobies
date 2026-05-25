c7644e617322e89fe3d6db06c642556a6624a8e9
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-05-25 21:26:11 +0530

    Configure dependency repositories for Android and Maven artifacts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,3 +19,17 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
