77c70ed24330e07c4f8e4122d3b320964ff00327
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-05-26 08:41:48 +0530

    Define the user-app:android module's android app package information from the version catalog

--- user-app/android/build.gradle.kts
+++ user-app/android/build.gradle.kts
@@ -14,4 +14,17 @@ kotlin {
     }
 }
 
+android {
+    namespace = libs.versions.userApp.android.namespace.get()
+    compileSdk = libs.versions.userApp.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = libs.versions.userApp.android.applicationId.get()
+        minSdk = libs.versions.userApp.android.minSdk.get().toInt()
+        targetSdk = libs.versions.userApp.android.targetSdk.get().toInt()
+        versionCode = libs.versions.userApp.android.versionCode.get().toInt()
+        versionName = libs.versions.userApp.android.versionName.get()
+    }
+}
+
 layout.buildDirectory.set(file("$rootDir/.build/user-app/android"))
