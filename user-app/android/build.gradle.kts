e89981635aeefc403d126b3f898a882e65e99b35
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-05-26 08:37:39 +0530

    Set Kotlin JVM target to JVM 11

--- user-app/android/build.gradle.kts
+++ user-app/android/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
@@ -5,5 +7,11 @@ plugins {
 
 group = libs.versions.userApp.android.group.get()
 version = libs.versions.userApp.android.version.get()
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
+}
 
 layout.buildDirectory.set(file("$rootDir/.build/user-app/android"))
