e0e1043bfea997b1f2ebde696347395ca52f44c3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-05-26 08:36:19 +0530

    Define the user-app:android module's configuration information from the version catalog

--- user-app/android/build.gradle.kts
+++ user-app/android/build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     alias(libs.plugins.compose.compiler)
 }
 
+group = libs.versions.userApp.android.group.get()
+version = libs.versions.userApp.android.version.get()
+
 layout.buildDirectory.set(file("$rootDir/.build/user-app/android"))
