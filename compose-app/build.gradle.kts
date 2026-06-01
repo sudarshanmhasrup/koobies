9799d73fc8bf1ff1ef7409f46ea401f498b186e5
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-06-01 09:50:26 +0530

    Add JVM desktop target to compose-app build configuration

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
@@ -9,6 +11,12 @@ group = libs.versions.composeApp.group.get()
 version = libs.versions.composeApp.version.get()
 
 kotlin {
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+
     android {
         namespace = libs.versions.composeApp.androidLibrary.namespace.get()
         compileSdk = libs.versions.composeApp.androidLibrary.compileSdk.get().toInt()
