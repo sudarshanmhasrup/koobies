9fe834b0447705e2fca37f0dd8970a577e049a8c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-06-01 08:30:14 +0530

    Apply the "org.jetbrains.kotlin.plugin.compose" plugin to the compose-app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
 }
 
 layout.buildDirectory.set(file("$rootDir/.build/compose-app"))
