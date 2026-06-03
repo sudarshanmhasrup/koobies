1bd675fcba5588c97cea1a1e453c47f676fd56b3
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-06-03 09:29:18 +0530

    Define the design-system:compose module's android library package information from the version catalog

--- design-system/compose/build.gradle.kts
+++ design-system/compose/build.gradle.kts
@@ -8,4 +8,12 @@ plugins {
 group = libs.versions.designSystem.compose.group.get()
 version = libs.versions.designSystem.compose.version.get()
 
-layout.buildDirectory.set(file("$rootDir/.build/compose-app"))
+kotlin {
+    android {
+        namespace = libs.versions.designSystem.compose.androidLibrary.namespace.get()
+        compileSdk = libs.versions.designSystem.compose.androidLibrary.compileSdk.get().toInt()
+        minSdk = libs.versions.designSystem.compose.androidLibrary.minSdk.get().toInt()
+    }
+}
+
+layout.buildDirectory.set(file("$rootDir/.build/design-system/compose"))
