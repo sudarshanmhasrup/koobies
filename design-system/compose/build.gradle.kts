0e74954bb58dd955d722152f010ac464bed758eb
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-06-03 09:33:17 +0530

    Add JVM desktop target to design-system compose module

--- design-system/compose/build.gradle.kts
+++ design-system/compose/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
@@ -9,6 +11,12 @@ group = libs.versions.designSystem.compose.group.get()
 version = libs.versions.designSystem.compose.version.get()
 
 kotlin {
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+
     android {
         namespace = libs.versions.designSystem.compose.androidLibrary.namespace.get()
         compileSdk = libs.versions.designSystem.compose.androidLibrary.compileSdk.get().toInt()
